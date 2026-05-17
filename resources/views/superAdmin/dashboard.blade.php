b7115beed18a6737348d76a8ad957f5473f7f58b
harris80-web <zurcsirrah80@gmail.com>
2026-05-17 18:43:18 +0800

    fixed css bug with vite

--- resources/views/superAdmin/dashboard.blade.php
+++ resources/views/superAdmin/dashboard.blade.php
@@ -5,8 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    @vite('resources/css/app.css')
-    @vite('resources/js/app.js')
 </head>
 
 <body class="flex gap-[20px]">
@@ -71,6 +69,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard | PLV-AlumNet</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="icon" type="image/x-icon" href="{{ asset('assets/PLV-AlumNet LOGO.png') }}">
     <script src="https://unpkg.com/lucide@latest"></script>
